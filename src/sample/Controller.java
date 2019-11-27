e4114970a7917d03a0696cdb56d71df3cdf4bf6a
Your Name <you@example.com>
2019-11-27 20:54:22 +0100

    rest of the 2D vector formulas

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
@@ -54,6 +55,18 @@ public class Controller implements Initializable {
     }
 
     public void calculateVectors(ActionEvent event) {
+        Model myModel = new Model();
+
+        double v1x = Double.parseDouble(tf_v1x.getText());
+        double v1y = Double.parseDouble(tf_v1y.getText());
+
+        double v2x = 0;
+        double v2y = 0;
+        if(!tf_v2x.getText().isEmpty() && !tf_v2y.getText().isEmpty()) {
+            v2x = Double.parseDouble(tf_v2x.getText());
+            v2y = Double.parseDouble(tf_v2y.getText());
+        }
+
         ToggleGroup toggleGroup = new ToggleGroup();
         rb_vectors1.setToggleGroup(toggleGroup);
         rb_vectors2.setToggleGroup(toggleGroup);
@@ -66,18 +79,37 @@ public class Controller implements Initializable {
         String option = checked.getText();
         switch (option) {
             case "Magnitude of v1":
-                lb_v1.setText("v2 = {" + tf_v1x.getText() + ", " + tf_v1y.getText() + "}");
-                lb_v2.setText("v1 = {0, 0}");
-                double v1x = Double.parseDouble(tf_v1x.getText());
-                double v1y = Double.parseDouble(tf_v1y.getText());
+                lb_v1.setText("v1 = {" + tf_v1x.getText() + ", " + tf_v1y.getText() + "}");
+                lb_v2.setText("v2 = {0, 0}");
                 lb_vectorsFormula.setText("[display formula]");
-                Model myModel = new Model();
                 double result = myModel.calculateMagnitude(v1x, v1y);
-                lb_vectorsResult.setText("Result " + result);
+                lb_vectorsResult.setText("m = " + result);
                 break;
-
+            case "Sum of v1 and v2":
+                lb_v1.setText("v1 = {" + tf_v1x.getText() + ", " + tf_v1y.getText() + "}");
+                lb_v2.setText("v2 = {" + tf_v2x.getText() + ", " + tf_v2y.getText() + "}");
+                lb_vectorsFormula.setText("[display formula]");
+                double[] sum = myModel.calculateSumOfTwoVectors(v1x, v1y, v2x, v2y);
+                lb_vectorsResult.setText("sum = " + Arrays.toString(sum));
+                break;
+            case "Dot product of v1 and v2":
+                lb_v1.setText("v1 = {" + tf_v1x.getText() + ", " + tf_v1y.getText() + "}");
+                lb_v2.setText("v2 = {" + tf_v2x.getText() + ", " + tf_v2y.getText() + "}");
+                lb_vectorsFormula.setText("[display formula]");
+                double[] vec1 = {v1x, v1y};
+                double[] vec2 = {v2x, v2y};
+                double dotProduct = myModel.calculateDotProductBetweenTwoVectors(vec1, vec2);
+                lb_vectorsResult.setText("v1 * v2 = " + dotProduct);
+                break;
+            case "Angle between v1 and v2":
+                lb_v1.setText("v1 = {" + tf_v1x.getText() + ", " + tf_v1y.getText() + "}");
+                lb_v2.setText("v2 = {" + tf_v2x.getText() + ", " + tf_v2y.getText() + "}");
+                lb_vectorsFormula.setText("[display formula]");
+                double cosA = myModel.calculateAngleBetweenTwoVectors(v1x, v1y, v2x, v2y);
+                lb_vectorsResult.setText("sum = " + cosA);
+                break;
             default:
-                System.out.println("Not an option, mate");
+                lb_v1.setText("Not an option, mate");
         }
 
     }
--- src/sample/Model.java
+++ src/sample/Model.java
@@ -14,4 +14,37 @@ public class Model
         //http://cljavacode.blogspot.com/2017/02/vector-magnitude.html
         return Math.sqrt(magnitude);
     }
+
+    public double[] calculateSumOfTwoVectors(double v1x, double v1y, double v2x, double v2y)
+    {
+        double v1 = v1x + v2x;
+        double v2 = v1y + v2y;
+        return new double[]{v1, v2};
+    }
+
+    public double calculateDotProductBetweenTwoVectors(double vect_A[], double vect_B[]) {
+        double product = 0;
+        int n = 2;
+
+        // Loop for calculate cot product
+        for (double i = 0; i < n; i++)
+            product = product + vect_A[(int) i] * vect_B[(int) i];
+        //https://www.geeksforgeeks.org/program-dot-product-cross-product-two-vector/
+        return product;
+    }
+
+    public double calculateAngleBetweenTwoVectors(double v1x, double v1y, double v2x, double v2y) {
+        //calculate dot product of two vectors
+        double vec1[] = {v1x, v1y};
+        double vec2[] = {v2x, v2y};
+        double dotProduct = calculateDotProductBetweenTwoVectors(vec1, vec2);
+
+        //calculate vectors magnitude
+        double mv1 = calculateMagnitude(v1x, v1y);
+        double mv2 = calculateMagnitude(v2x, v2y);
+
+        //calculate angle between vectores
+        double cosA = dotProduct / (mv1 * mv2);
+        return cosA;
+    }
 }
